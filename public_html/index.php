531e149dae36f7e2619f8304bcc289ffa85983e9
Valente Meza <kingracoon64@gmail.com>
2017-05-14 00:19:19 -0600

    html tags added style.css added.

---
+++ public_html/index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- the 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta charset="utf-8"/>
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+    <!-- latest compiled and minified css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-bvyiisifek1dgmjrakycuhahrg32omucww7on3rydg4va+pmstsz/k68vbdejh4u" crossorigin="anonymous">
+
+    <!-- optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rhyon1irsvxv4nd0jutlngaslcjuc7uwjduw9svrlvryoopp2bwygmgjqixwl/sp" crossorigin="anonymous">
+
+    <!-- latest compiled and minified javascript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-tc5iqib027qvyjsmfhjomalkfuwvxzxupncja7l2mcwnipg9mgcd8wgnicpd7txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+    <title>Baca Fight Systems</title>
+</head>
+<body class="sfooter">
+<di class="sfooter-content">
+
+    <!--begin header and nabar-->
+<header>
+    <div class="container">
+        <!--Begin the Bootstrap Navbar - see documentation: http://getbootstrap.com/components/#navbar -->
+        <nav class="navbar navbar-inverse">
+            <div class="navbar-header">
+                <a class="navbar-brand">Baca Fight Systems.</a>
+            </div>
+    </div>
+</header>
+
+    <!--begin welcome row-->
+<main>
+    <!--begin welcome row-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="content">
+                Baca Fight Systems
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+   <footer>
+       <div class="container">
+       </div>
+   </footer>
+</body>
+</html>
